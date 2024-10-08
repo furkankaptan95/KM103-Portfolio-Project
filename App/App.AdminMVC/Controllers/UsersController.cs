1627d59f123dfdd3ca92e2c26f357c97db6bd5ad
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-08 22:32:23 +0300

    UsersController action ve viewlar oluşturuldu.

---
+++ App/App.AdminMVC/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.AdminMVC.Controllers;
+public class UsersController : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> User()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> AllUsers()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> AddUser()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddUser([FromForm] object addUserModel)
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> UpdateUser()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UpdateUser([FromForm] object updateUserModel)
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> DeleteUser()
+    {
+        return View();
+    }
+
+}
