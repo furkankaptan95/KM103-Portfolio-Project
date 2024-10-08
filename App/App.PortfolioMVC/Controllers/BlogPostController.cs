5e17522cdc33889c033114396bad48eaa9cc3193
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-08 17:08:11 +0300

    PortfolioMVC ve Clark Template entegrasyonu

---
+++ App/App.PortfolioMVC/Controllers/BlogPostController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.PortfolioMVC.Controllers;
+public class BlogPostController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
