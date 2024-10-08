0bbfc91445c194ca7b56997c69433cacfd2e4e46
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-09 02:40:00 +0300

    AboutMe view düzenlenmesi
    about me viewı için fake data controller actionu ile gönderildi,view düzenlendi. About me viewı için model oluşturuldu.

--- App/App.AdminMVC/Controllers/AboutMeController.cs
+++ App/App.AdminMVC/Controllers/AboutMeController.cs
@@ -8,7 +8,14 @@ public class AboutMeController : Controller
     [HttpGet]
     public async Task<IActionResult> AboutMe()
     {
-        return View();
+
+        var aboutMeModel = new AboutMeViewModel();
+
+        aboutMeModel.Introduction = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
+        aboutMeModel.ImageUrl1 = "default-img.jpg";
+        aboutMeModel.ImageUrl2 = "default-img.jpg";
+
+        return View(aboutMeModel);
     }
 
     [HttpGet]
