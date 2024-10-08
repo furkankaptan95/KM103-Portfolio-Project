baee69f191dcdd80e5dcad81c6511cb3ab0393fe
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-08 21:33:11 +0300

    BlogPostsController işlemleri ve fazlası
    BlogPostsController crud action ve viewları oluturuldu. ExperiencesController'a daha önce eklemeyi unuttuğum 2 yeni action daha eklendi.

---
+++ App/App.AdminMVC/Controllers/BlogPostsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.AdminMVC.Controllers;
+public class BlogPostsController : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> BlogPost()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> AllBlogPosts()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> AddBlogPost()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddBlogPost([FromForm] object addBlogPostModel)
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> UpdateBlogPost()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UpdateBlogPost([FromForm] object updateBlogPostModel)
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> DeleteBlogPost()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> MakeBlogPostVisible()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> MakeBlogPostInVisible()
+    {
+        return View();
+    }
+}
