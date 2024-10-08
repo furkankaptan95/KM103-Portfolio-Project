79dd271f8f9a545d33f295cc3f040de119ff8c3b
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-08 21:22:20 +0300

    AboutMeController action ve viewları eklendi.

---
+++ App/App.AdminMVC/Controllers/AboutMeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.AdminMVC.Controllers;
+public class AboutMeController : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> AboutMe()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> AddAboutMe()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddAboutMe([FromForm] object addAboutMeModel)
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> UpdateAboutMe()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UpdateAboutMe([FromForm] object updateAboutMeModel)
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> DeleteAboutMe()
+    {
+        return View();
+    }
+}
