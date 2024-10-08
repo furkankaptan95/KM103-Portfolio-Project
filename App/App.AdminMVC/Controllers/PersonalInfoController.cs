59df572a6b7d704f863ea901734e04e6d06a77ca
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-08 22:20:53 +0300

    PersonalInfoController action ve viewlar eklendi.

---
+++ App/App.AdminMVC/Controllers/PersonalInfoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.AdminMVC.Controllers;
+public class PersonalInfoController : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> PersonalInfo()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> AddPersonalInfo()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddPersonalInfo([FromForm] object addPersonalInfoModel)
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> UpdatePersonalInfo()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UpdatePersonalInfo([FromForm] object updatePersonalInfoModel)
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> DeletePersonalInfo()
+    {
+        return View();
+    }
+}
