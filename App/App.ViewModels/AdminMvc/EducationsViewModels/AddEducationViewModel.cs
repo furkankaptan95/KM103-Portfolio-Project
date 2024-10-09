675a5059d1034e2944db890290887068d11ee1ed
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-09 21:46:03 +0300

    AddEducationViewModel oluşturuldu.

---
+++ App/App.ViewModels/AdminMvc/EducationsViewModels/AddEducationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.ViewModels.AdminMvc.EducationsViewModels;
+public class AddEducationViewModel
+{
+    [Required(ErrorMessage = "Derece kısmı zorunludur.")]
+    [MaxLength(100, ErrorMessage = "Derece kısmı en fazla 100 karakter olabilir.")]
+    public string Degree { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Okul kısmı zorunludur.")]
+    [MaxLength(100, ErrorMessage = "Okul kısmı en fazla 100 karakter olabilir.")]
+    public string School { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Başlangıç tarihi zorunludur.")]
+    public DateTime StartDate { get; set; }
+    [Required(ErrorMessage = "Bitiş tarihi zorunludur.")]
+    public DateTime EndDate { get; set; }
+
+}
+
