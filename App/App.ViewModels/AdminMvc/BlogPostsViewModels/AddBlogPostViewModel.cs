97f0de7fa46d9693c0792d407e4cefd703870358
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-09 16:58:40 +0300

    AddBlogPost view düzenlemesi
    Blog Post eklemek için gerekli form ve post ekleme modeli oluşturuldu.

---
+++ App/App.ViewModels/AdminMvc/BlogPostsViewModels/AddBlogPostViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace App.ViewModels.AdminMvc.BlogPostsViewModels;
+public class AddBlogPostViewModel
+{
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+}
