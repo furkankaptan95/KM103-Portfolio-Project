e3c0cf76183e31d8b6dfcbbea26b02279f533c53
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-10 00:17:43 +0300

    AddExperienceViewModel oluşturuldu.

---
+++ App/App.ViewModels/AdminMvc/ExperiencesViewModels/AddExperienceViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.ViewModels.AdminMvc.ExperiencesViewModels;
+public class AddExperienceViewModel
+{
+    [Required(ErrorMessage = "Başlık kısmı zorunludur.")]
+    [MaxLength(100, ErrorMessage = "Başlık kısmı en fazla 100 karakter olabilir.")]
+    public string Title { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Firma kısmı zorunludur.")]
+    [MaxLength(100, ErrorMessage = "Firma kısmı en fazla 100 karakter olabilir.")]
+    public string Company { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Açıklama kısmı zorunludur.")]
+    [MaxLength(600, ErrorMessage = "Açıklama kısmı en fazla 600 karakter olabilir.")]
+    public string Description { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Başlangıç tarihi zorunludur.")]
+    [DataType(DataType.Date)]
+    public DateTime StartDate { get; set; }
+
+    [Required(ErrorMessage = "Bitiş tarihi zorunludur.")]
+    [DataType(DataType.Date)]
+    public DateTime EndDate { get; set; }
+}
