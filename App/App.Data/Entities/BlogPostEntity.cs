0c2506da633cff641b58221499d19c3f3d248ffe
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-09 17:41:58 +0300

    BlogPostEntitye IsVisible alanı eklendi.

--- App/App.Data/Entities/BlogPostEntity.cs
+++ App/App.Data/Entities/BlogPostEntity.cs
@@ -5,5 +5,6 @@ public class BlogPostEntity : BaseEntity<int>
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
     public DateTime PublishDate { get; set; }
+    public bool IsVisible { get; set; } = true;
     public virtual ICollection<CommentEntity> Comments { get; set; } = new List<CommentEntity>();
 }
