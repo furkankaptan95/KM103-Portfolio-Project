17a4f263662eaf75a558915b395f0a266c744774
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-10 17:23:27 +0300

    AllProject view tablosu oluşturuldu.

--- App/App.Data/Entities/ProjectEntity.cs
+++ App/App.Data/Entities/ProjectEntity.cs
@@ -5,5 +5,6 @@ public class ProjectEntity : BaseEntity<int>
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string ImageUrl { get; set; } = string.Empty;
+    public bool IsVisible { get; set; } = true;
 
 }
--- App/App.ViewModels/AdminMvc/ProjectsViewModels/AllProjectsViewModel.cs
+++ App/App.ViewModels/AdminMvc/ProjectsViewModels/AllProjectsViewModel.cs
@@ -5,4 +5,5 @@ public class AllProjectsViewModel
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string ImageUrl { get; set; } = string.Empty;
+    public bool IsVisible { get; set; }
 }
