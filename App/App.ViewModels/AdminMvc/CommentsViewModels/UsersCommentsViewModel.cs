894bebbc12dce64cbd3424da320edf40da493123
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-10 21:28:28 +0300

    View Modeller oluşturuldu.
    AllUsersViewModel ve UsersCommentsViewModel modelleri oluşturuldu.

---
+++ App/App.ViewModels/AdminMvc/CommentsViewModels/UsersCommentsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace App.ViewModels.AdminMvc.CommentsViewModels;
+public class UsersCommentsViewModel
+{
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public bool IsApproved { get; set; } = false;
+    public string BlogPostName { get; set; } = string.Empty;
+}
---
+++ App/App.ViewModels/AdminMvc/UsersViewModels/AllUsersViewModel.cs
@@ -0,0 +1,12 @@
+﻿using App.ViewModels.AdminMvc.CommentsViewModels;
+
+namespace App.ViewModels.AdminMvc.UsersViewModels;
+public class AllUsersViewModel
+{
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+    public string? ImageUrl { get; set; } = string.Empty;
+    public List<UsersCommentsViewModel> Comments { get; set; } = new();
+
+}
