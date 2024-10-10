eb5f6eae75ffa8eecb73a1dcc8c849597ae0c192
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-10 17:13:52 +0300

    AllProjectsViewModel oluşturuldu

---
+++ App/App.ViewModels/AdminMvc/ProjectsViewModels/AllProjectsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace App.ViewModels.AdminMvc.ProjectsViewModels;
+public class AllProjectsViewModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+}
