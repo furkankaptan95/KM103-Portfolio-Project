a4ac7521010d6ea950ce0a67c6b97bf777117ff7
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-10 22:29:28 +0300

    AllCommentsViewModel oluşturuldu.

---
+++ App/App.ViewModels/AdminMvc/CommentsViewModels/AllCommentsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace App.ViewModels.AdminMvc.CommentsViewModels;
+public class AllCommentsViewModel
+{
+    public int Id { get; set; }
+    public string Commenter { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public bool IsApproved { get; set; } = false;
+    public string BlogPostName { get; set; } = string.Empty;
+}
