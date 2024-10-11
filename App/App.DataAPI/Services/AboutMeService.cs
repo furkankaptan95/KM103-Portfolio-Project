05a549363fdb836475d3396b1e4d0ec1fa3fe736
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-11 08:15:13 +0300

    DataApiDbContext service'e eklendi.

--- App/App.DataAPI/Services/AboutMeService.cs
+++ App/App.DataAPI/Services/AboutMeService.cs
@@ -1,6 +1,25 @@
-﻿namespace App.DataAPI.Services
+﻿using App.Data.DbContexts;
+using App.DTOs.AboutMeDtos;
+using App.Services.AdminServices.Abstract;
+using Ardalis.Result;
+
+namespace App.DataAPI.Services
 {
-    public class AboutMeService
+    public class AboutMeService(DataApiDbContext dataApiDb) : IAboutMeService
     {
+        public Task<Result> AddAboutMeAsync(AddAboutMeDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<ShowAboutMeDto>> GetAboutMeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> UpdateAboutMeAsync(UpdateAboutMeDto dto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
