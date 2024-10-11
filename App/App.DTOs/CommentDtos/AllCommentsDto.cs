60db50795711cfe7f1f7921000d23bf9a913e405
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-11 04:31:49 +0300

    Dto oluşturuldu.

---
+++ App/App.DTOs/CommentDtos/AllCommentsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace App.DTOs.CommentDtos;
+public class AllCommentsDto
+{
+    public int Id { get; set; }
+    public string Commenter { get; set; } 
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public bool IsApproved { get; set; }
+    public string BlogPostName { get; set; }
+}
