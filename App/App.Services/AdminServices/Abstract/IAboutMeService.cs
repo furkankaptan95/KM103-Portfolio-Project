15b17e0d70c1efc526d47c6c9a1a580c7bd5b0d5
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-11 04:26:25 +0300

    Methdolarda düzenlemeler yapıldı.

--- App/App.Services/AdminServices/Abstract/IAboutMeService.cs
+++ App/App.Services/AdminServices/Abstract/IAboutMeService.cs
@@ -4,7 +4,7 @@ using Ardalis.Result;
 namespace App.Services.AdminServices.Abstract;
 public interface IAboutMeService
 {
-    Task<Result<AddAboutMeDto>> AddAboutMeAsync();
+    Task<Result> AddAboutMeAsync(AddAboutMeDto dto);
     Task<Result<ShowAboutMeDto>> GetAboutMeAsync();
-    Task<Result<UpdateAboutMeDto>> UpdateAboutMeAsync();
+    Task<Result> UpdateAboutMeAsync(UpdateAboutMeDto dto);
 }
