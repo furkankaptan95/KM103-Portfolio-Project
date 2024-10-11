c9af2b3880fa835f82218932917679676e87d5c0
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-11 18:02:13 +0300

    AboutMeFileApiDto eklendi.
    MVC ile FileApi arasında veeri aktarımı sırasında kullanılacak olan Dto oluşturuldu.

---
+++ App/App.DTOs/AboutMeDtos/AboutMeFileApiDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace App.DTOs.AboutMeDtos;
+public class AboutMeFileApiDto
+{
+    public string? ImageUrl1 { get; set; }
+    public string? ImageUrl2 { get; set; }
+    public IFormFile? ImageFile1 { get; set; }
+    public IFormFile? ImageFile2 { get; set; }
+}
