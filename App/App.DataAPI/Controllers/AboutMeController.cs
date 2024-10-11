5252926a8fc64ae88363d71a5641ec88f76d2efb
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-11 08:19:54 +0300

    IAboutMeService primary constructor ile controllera eklendi.

--- App/App.DataAPI/Controllers/AboutMeController.cs
+++ App/App.DataAPI/Controllers/AboutMeController.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using App.Services.AdminServices.Abstract;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.DataAPI.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class AboutMeController : ControllerBase
+public class AboutMeController(IAboutMeService aboutMeService) : ControllerBase
 {
     [HttpGet("/get-about-me")]
     public async Task<IActionResult> GetAboutMeAsync()
