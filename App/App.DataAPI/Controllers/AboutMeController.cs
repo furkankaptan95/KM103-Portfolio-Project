fe0c72b06a49987c90b7d4e7ecb588ef22403f52
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-11 08:10:30 +0300

    Controllerda Get actionu eklendi.

---
+++ App/App.DataAPI/Controllers/AboutMeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.DataAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AboutMeController : ControllerBase
+{
+    [HttpGet("/get-about-me")]
+    public async Task<IActionResult> GetAboutMeAsync()
+    {
+        return Ok();
+    }
+}
