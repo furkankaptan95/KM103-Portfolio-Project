ff949aff9a728fb6b632434eb9b442ad30382874
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-11 18:55:21 +0300

    FileController oluşturuldu.
    FileController eklendi, constructor ile dosya yolu oluşturuldu.

---
+++ App/App.FileAPI/Controllers/FileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.FileAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FileController : ControllerBase
+{
+    private readonly string _uploadsFolder;
+
+    public FileController()
+    {
+        // Uploads dizininin yolunu belirle
+        _uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
+
+        // Eğer uploads dizini yoksa oluştur
+        if (!Directory.Exists(_uploadsFolder))
+        {
+            Directory.CreateDirectory(_uploadsFolder);
+        }
+    }
+}
