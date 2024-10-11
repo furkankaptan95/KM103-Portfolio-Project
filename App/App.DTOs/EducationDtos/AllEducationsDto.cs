e646b2bbecfea60db14cf78227ef03d413b6ebfc
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-11 04:39:18 +0300

    Dtolar eklendi.

---
+++ App/App.DTOs/EducationDtos/AllEducationsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace App.DTOs.EducationDtos;
+public class AllEducationsDto
+{
+    public int Id { get; set; }
+    public string Degree { get; set; }
+    public string School { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public bool IsVisible { get; set; }
+}
