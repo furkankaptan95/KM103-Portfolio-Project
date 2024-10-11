a5a1801a65838306789a7691778ab2447e2d23e4
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-11 19:05:55 +0300

    UploadFile actionu ve Dto düzenlemeleri yapıldı.
    UploadFile actionu oluşturulup düzenlendi, Upload edilecek file dosyaları iin ortak bir dto oluştuuldu ve yine geri döndürülecek url ksımları için de ayrı bir dto oluşturuldu.

---
+++ App/App.DTOs/FileApiDtos/UploadFileDto.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace App.DTOs.FileApiDtos;
+public class UploadFileDto
+{
+    public IFormFile ImageFile1 { get; set; }
+    public IFormFile? ImageFile2 { get; set; }
+}
--- App/App.FileAPI/Controllers/FileController.cs
+++ App/App.FileAPI/Controllers/FileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using App.DTOs.FileApiDtos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.FileAPI.Controllers;
@@ -19,5 +20,35 @@ public class FileController : ControllerBase
         {
             Directory.CreateDirectory(_uploadsFolder);
         }
+    }
+
+    [HttpPost("upload-files")]
+    public async Task<IActionResult> UploadFilesAsync([FromBody] UploadFileDto dto)
+    {
+     
+        // İlk dosyayı kaydet
+        var fileName1 = Path.GetFileName(dto.ImageFile1.FileName);
+        var filePath1 = Path.Combine(_uploadsFolder, fileName1);
+        using (var stream = new FileStream(filePath1, FileMode.Create))
+        {
+            await dto.ImageFile1.CopyToAsync(stream);
+        }
+
+        // İkinci dosyayı kaydet
+        var fileName2 = Path.GetFileName(dto.ImageFile2.FileName);
+        var filePath2 = Path.Combine(_uploadsFolder, fileName2);
+        using (var stream = new FileStream(filePath2, FileMode.Create))
+        {
+            await dto.ImageFile2.CopyToAsync(stream);
+        }
+
+        var urlDto = new ReturnUrlDto
+        {
+            ImageUrl1 = fileName1,
+            ImageUrl2 = fileName2,
+        };
+
+        // Başarılı yanıt döndür
+        return Ok(urlDto);
     }
 }
