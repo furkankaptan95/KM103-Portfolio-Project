d6fd8ae9fd039f4bf8b963cf54a04c1906733afb
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-12 17:51:58 +0300

    AboutMeReturnUrlDtoValidator ve AddAboutMeApiDtoValidator oluşturuldu.

---
+++ App/App.Core/Validators/AboutMeValidators/AboutMeReturnUrlDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using App.DTOs.FileApiDtos;
+using FluentValidation;
+
+namespace App.Core.Validators.AboutMeValidators;
+public class AboutMeReturnUrlDtoValidator : AbstractValidator<ReturnUrlDto>
+{
+    public AboutMeReturnUrlDtoValidator()
+    {
+        RuleFor(x => x.ImageUrl1).NotEmpty().WithMessage("Image1Url boş olamaz.");
+
+        RuleFor(x => x.ImageUrl2).NotEmpty().WithMessage("Image1Url boş olamaz.");
+    }
+}
---
+++ App/App.Core/Validators/AboutMeValidators/AddAboutMeApiDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using App.DTOs.AboutMeDtos;
+using FluentValidation;
+
+namespace App.Core.Validators.AboutMeValidators;
+public class AddAboutMeApiDtoValidator : AbstractValidator<AddAboutMeApiDto>
+{
+    public AddAboutMeApiDtoValidator()
+    {
+        RuleFor(x => x.ImageUrl1).NotEmpty().WithMessage("Image1Url boş olamaz.");
+
+        RuleFor(x => x.ImageUrl2).NotEmpty().WithMessage("Image1Url boş olamaz.");
+
+        RuleFor(x => x.Introduction)
+            .NotEmpty().WithMessage("Giriş kısmı boş olamaz.")
+             .Length(10, 1000).WithMessage("Başlık 10 ile 1000 karakter arasında olmalı.");
+    }
+}
