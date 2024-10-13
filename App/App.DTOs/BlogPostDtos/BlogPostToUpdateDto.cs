b759f15b730e1590cf188078c9d190db31a389cd
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-14 00:54:03 +0300

    View'a gönderilecek model için dto oluşturuldu.

---
+++ App/App.DTOs/BlogPostDtos/BlogPostToUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace App.DTOs.BlogPostDtos;
+
+public class BlogPostToUpdateDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+}
