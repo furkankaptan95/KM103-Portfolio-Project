d7eb315f50dfb45efa82c36492d607f5266fe953
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-14 00:57:08 +0300

    BlogPostEntity UpdateAt
    BlogPostEntity'e UpdateAt özelliği eklendi, konfigürasyonu da eklendi.

--- App/App.Data/Entities/BlogPostEntity.cs
+++ App/App.Data/Entities/BlogPostEntity.cs
@@ -5,6 +5,7 @@ public class BlogPostEntity : BaseEntity<int>
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
     public DateTime PublishDate { get; set; }
+    public DateTime? UpdatedAt { get; set; }
     public bool IsVisible { get; set; } = true;
     public virtual ICollection<CommentEntity> Comments { get; set; } = new List<CommentEntity>();
 }
