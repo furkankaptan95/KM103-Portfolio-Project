7c45c239c4550742840326bc3462bdb934bd9e5f
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-14 16:11:49 +0300

    API'ye Education Service eklendi.

---
+++ App/App.DataAPI/Services/EducationService.cs
@@ -0,0 +1,32 @@
+﻿using App.DTOs.EducationDtos;
+using App.Services.AdminServices.Abstract;
+using Ardalis.Result;
+
+namespace App.DataAPI.Services;
+public class EducationService : IEducationService
+{
+    public Task<Result> AddEducationAsync(AddEducationDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result> ChangeEducationVisibilityAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result> DeleteEducationAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result<List<AllEducationsDto>>> GetAllEducationsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result> UpdateEducationAsync(UpdateEducationDto dto)
+    {
+        throw new NotImplementedException();
+    }
+}
