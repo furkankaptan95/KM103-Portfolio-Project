23630d12b381318d031389e7d30cb1235f0f662b
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-14 19:09:39 +0300

    EducationToUpdateDto oluşturuldu.

---
+++ App/App.DTOs/EducationDtos/EducationToUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DTOs.EducationDtos;
+public class EducationToUpdateDto
+{
+    public int Id { get; set; }
+    public string Degree { get; set; }
+    public string School { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+}
