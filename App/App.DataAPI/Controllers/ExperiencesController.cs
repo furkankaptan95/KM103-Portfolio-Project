58710f98818e26286b0c76495ce2ef7006c1f9d4
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-15 01:50:27 +0300

    Experience API Controller ChangeVisibility actionu düzenlendi.

--- App/App.DataAPI/Controllers/ExperiencesController.cs
+++ App/App.DataAPI/Controllers/ExperiencesController.cs
@@ -94,4 +94,21 @@ public class ExperiencesController : ControllerBase
 
         return Ok(result);
     }
+
+
+    [HttpGet("/change-experience-visibility-{id:int}")]
+    public async Task<IActionResult> ChangeVisibilityAsync([FromRoute] int id)
+    {
+        var result = await _experiencesService.ChangeExperienceVisibilityAsync(id);
+
+        if (!result.IsSuccess)
+        {
+            if (result.Status == ResultStatus.NotFound)
+            {
+                return NotFound(result);
+            }
+            return StatusCode(500, result);
+        }
+        return Ok(result);
+    }
 }
