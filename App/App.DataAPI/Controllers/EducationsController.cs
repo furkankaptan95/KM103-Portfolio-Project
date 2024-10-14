677c8cb770a061374663d6bc4310053947d337eb
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-14 16:16:12 +0300

    API EducationsController ve GetAll actionu oluşturuldu.

---
+++ App/App.DataAPI/Controllers/EducationsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.DataAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EducationsController : ControllerBase
+{
+    [HttpGet("/get-all-educations")]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok();
+    }
+}
