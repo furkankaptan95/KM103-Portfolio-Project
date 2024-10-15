8fc48566624fc3589734cdaddb6169163ff88c74
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-15 19:47:16 +0300

    IProjectService düzenlemesi , servislerin API ve MVCye eklenmesi yapıldı.

--- App/App.AdminMVC/Program.cs
+++ App/App.AdminMVC/Program.cs
@@ -36,6 +36,8 @@ builder.Services.AddScoped<IBlogPostService, BlogPostService>();
 builder.Services.AddScoped<IEducationService, EducationService>();
 builder.Services.AddScoped<IExperienceService, ExperienceService>();
 builder.Services.AddScoped<IPersonalInfoService, PersonalInfoService>();
+builder.Services.AddScoped<IProjectService, ProjectService>();
+
 
 var app = builder.Build();
 
--- App/App.DataAPI/Program.cs
+++ App/App.DataAPI/Program.cs
@@ -33,6 +33,7 @@ builder.Services.AddScoped<IBlogPostService, BlogPostService>();
 builder.Services.AddScoped<IEducationService, EducationService>();
 builder.Services.AddScoped<IExperienceService, ExperienceService>();
 builder.Services.AddScoped<IPersonalInfoService, PersonalInfoService>();
+builder.Services.AddScoped<IProjectService, ProjectService>();
 
 
 builder.Services.AddTransient<IValidator<AddAboutMeApiDto>, AddAboutMeApiDtoValidator>();
