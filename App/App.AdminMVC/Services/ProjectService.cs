10a4b20013d62316cc54ec441858768027c0e22e
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-15 22:21:12 +0300

    Project MVC Service Add metodu düzenlendi.

--- App/App.AdminMVC/Services/ProjectService.cs
+++ App/App.AdminMVC/Services/ProjectService.cs
@@ -1,4 +1,6 @@
-﻿using App.DTOs.ExperienceDtos;
+﻿using App.DTOs.AboutMeDtos;
+using App.DTOs.ExperienceDtos;
+using App.DTOs.FileApiDtos;
 using App.DTOs.ProjectDtos;
 using App.Services.AdminServices.Abstract;
 using Ardalis.Result;
@@ -7,15 +9,45 @@ namespace App.AdminMVC.Services;
 public class ProjectService(IHttpClientFactory factory) : IProjectService
 {
     private HttpClient DataApiClient => factory.CreateClient("dataApi");
+    private HttpClient FileApiClient => factory.CreateClient("fileApi");
 
     public Task<Result> AddProjectAsync(AddProjectApiDto dto)
     {
         throw new NotImplementedException();
     }
 
-    public Task<Result> AddProjectAsync(AddProjectMVCDto dto)
+    public async Task<Result> AddProjectAsync(AddProjectMVCDto dto)
     {
-        throw new NotImplementedException();
+        using var content = new MultipartFormDataContent();
+
+        var imageContent = new StreamContent(dto.ImageFile.OpenReadStream());
+        imageContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(dto.ImageFile.ContentType);
+        content.Add(imageContent, "imageFile1", dto.ImageFile.FileName); // "imageFile1" API'deki parametre adı ile uyumlu olmalı
+
+        var fileApiResponse = await FileApiClient.PostAsync("upload-files", content);
+
+        if (!fileApiResponse.IsSuccessStatusCode)
+        {
+            return Result.Error("Resimler yüklenirken beklenmeyen bir hata oluştu.");
+        }
+
+        var urlDto = await fileApiResponse.Content.ReadFromJsonAsync<ReturnUrlDto>();
+
+        var apiDto = new AddProjectApiDto
+        {
+            ImageUrl = urlDto.ImageUrl1,
+            Title = dto.Title,
+            Description = dto.Description,
+        };
+
+        var apiResponse = await DataApiClient.PostAsJsonAsync("add-project", apiDto);
+
+        if (apiResponse.IsSuccessStatusCode)
+        {
+            return Result.SuccessWithMessage("Proje  başarıyla eklendi. ");
+        }
+
+        return Result.Error("Proje eklenirken beklenmeyen bir hata oluştu..");
     }
 
     public Task<Result> ChangeProjectVisibilityAsync(int id)
