5ae16bb66567cc2ed9d43eceaa0abaf3a99c6805
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-15 04:22:31 +0300

    PersonalInfo API Service Add metodu düzenlendi.

--- App/App.DataAPI/Services/PersonalInfoService.cs
+++ App/App.DataAPI/Services/PersonalInfoService.cs
@@ -1,13 +1,38 @@
-﻿using App.DTOs.PersonalInfoDtos;
+﻿using App.Data.DbContexts;
+using App.Data.Entities;
+using App.DTOs.PersonalInfoDtos;
 using App.Services.AdminServices.Abstract;
 using Ardalis.Result;
+using Microsoft.EntityFrameworkCore;
 
 namespace App.DataAPI.Services;
-public class PersonalInfoService : IPersonalInfoService
+public class PersonalInfoService(DataApiDbContext dataApiDb) : IPersonalInfoService
 {
-    public Task<Result> AddPersonalInfoAsync(AddPersonalInfoDto dto)
+    public async Task<Result> AddPersonalInfoAsync(AddPersonalInfoDto dto)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var entity = new PersonalInfoEntity()
+            {
+                About = dto.About,
+                Name = dto.Name,
+                Surname = dto.Surname,
+                BirthDate = dto.BirthDate,
+            };
+
+            await dataApiDb.PersonalInfos.AddAsync(entity);
+            await dataApiDb.SaveChangesAsync();
+
+            return Result.Success();
+        }
+        catch (DbUpdateException dbEx)
+        {
+            return Result.Error("Veritabanı hatası: " + dbEx.Message);
+        }
+        catch (Exception ex)
+        {
+            return Result.Error("Bir hata oluştu: " + ex.Message);
+        }
     }
 
     public Task<Result<ShowPersonalInfoDto>> GetPersonalInfoAsync()
