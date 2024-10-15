96f68cb20310e826f92d420f359de1a76c875335
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-15 19:26:33 +0300

    ProjectEntity düzenlendi.

--- App/App.Data/Entities/ProjectEntity.cs
+++ App/App.Data/Entities/ProjectEntity.cs
@@ -2,9 +2,9 @@
 namespace App.Data.Entities;
 public class ProjectEntity : BaseEntity<int>
 {
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public string ImageUrl { get; set; } = string.Empty;
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
     public bool IsVisible { get; set; } = true;
 
 }
