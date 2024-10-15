7bfa440ab9732c1cdcb7ba32b8eebf8cb140240c
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-15 20:16:51 +0300

    Project API Contoller GetAll actionu düzenlendi.

--- App/App.DataAPI/Controllers/ProjectsController.cs
+++ App/App.DataAPI/Controllers/ProjectsController.cs
@@ -1,4 +1,5 @@
 ﻿using App.DTOs.ProjectDtos;
+using App.Services.AdminServices.Abstract;
 using FluentValidation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,10 +12,24 @@ public class ProjectsController : ControllerBase
 {
     private readonly IValidator<UpdateProjectApiDto> _updateValidator;
     private readonly IValidator<AddProjectApiDto> _addValidator;
-
-    public ProjectsController(IValidator<UpdateProjectApiDto> updateValidator, IValidator<AddProjectApiDto> addValidator)
+    private readonly IProjectService _projectService;
+    public ProjectsController(IValidator<UpdateProjectApiDto> updateValidator, IValidator<AddProjectApiDto> addValidator, IProjectService projectService)
     {
         _addValidator = addValidator;
         _updateValidator = updateValidator;
+        _projectService = projectService;
+    }
+
+    [HttpGet("/all-experiences")]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var result = await _projectService.GetAllProjectsAsync();
+
+        if (!result.IsSuccess)
+        {
+            return StatusCode(500, result);
+        }
+
+        return Ok(result);
     }
 }
