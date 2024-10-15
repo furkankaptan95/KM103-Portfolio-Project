a75111b8464d76935c6225ed820f99b67050d660
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-16 01:39:48 +0300

    Project API Contoller HomeInfos actionu düzenlendi.

--- App/App.DataAPI/Program.cs
+++ App/App.DataAPI/Program.cs
@@ -36,6 +36,7 @@ builder.Services.AddScoped<IEducationService, EducationService>();
 builder.Services.AddScoped<IExperienceService, ExperienceService>();
 builder.Services.AddScoped<IPersonalInfoService, PersonalInfoService>();
 builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<IHomeService, HomeService>();
 
 
 builder.Services.AddTransient<IValidator<AddAboutMeApiDto>, AddAboutMeApiDtoValidator>();
