e67069734daf22a64a44d4914be3a568231ea8f0
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-16 20:49:48 +0300

    Admin AllComments DataAPI Controller işlemleri yapıldı.

--- App/App.DataAPI/Controllers/CommentsController.cs
+++ App/App.DataAPI/Controllers/CommentsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using App.Services.AdminServices.Abstract;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.DataAPI.Controllers;
@@ -7,4 +8,23 @@ namespace App.DataAPI.Controllers;
 [ApiController]
 public class CommentsController : ControllerBase
 {
+    private readonly ICommentService _commentService;
+
+    public CommentsController(ICommentService commentService)
+    {
+        _commentService = commentService;
+    }
+
+    [HttpGet("/all-comments")]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var result = await _commentService.GetAllCommentsAsync();
+
+        if (!result.IsSuccess)
+        {
+            return StatusCode(500, result);
+        }
+
+        return Ok(result);
+    }
 }
