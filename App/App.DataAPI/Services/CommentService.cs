0aff23170171c18e9cb8ea67ae6afc9c870b0264
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-16 22:53:26 +0300

    UsersComment DataApi Service metodu düzenlendi.

--- App/App.DataAPI/Services/CommentService.cs
+++ App/App.DataAPI/Services/CommentService.cs
@@ -1,5 +1,7 @@
 ﻿using App.Data.DbContexts;
 using App.DTOs.CommentDtos;
+using App.DTOs.EducationDtos;
+using App.DTOs.UserDtos;
 using App.Services.AdminServices.Abstract;
 using Ardalis.Result;
 using Microsoft.Data.SqlClient;
@@ -142,6 +144,42 @@ public class CommentService : ICommentService
         catch (Exception ex)
         {
             return Result<List<AllCommentsDto>>.Error("Bir hata oluştu: " + ex.Message);
+        }
+    }
+
+    public async Task<Result<List<UsersCommentsDto>>> GetUsersCommentsAsync(int id)
+    {
+        try
+        {
+            var usersComments = new List<UsersCommentsDto>();
+
+            var comments = await _dataApiDb.Comments.Where(c=>c.UserId == id).Include(c=>c.BlogPost).ToListAsync();
+
+            if(comments.Count > 0)
+            {        
+                foreach (var comment in comments)
+                {
+                    UsersCommentsDto dto = new();
+
+                    dto.BlogPostName = comment.BlogPost.Title;
+                    dto.Content = comment.Content;
+                    dto.CreatedAt = comment.CreatedAt;
+                    dto.IsApproved = comment.IsApproved;
+
+                    usersComments.Add(dto);
+                }
+            }
+
+            return Result<List<UsersCommentsDto>>.Success(usersComments);
+        }
+
+        catch (SqlException sqlEx)
+        {
+            return Result<List<UsersCommentsDto>>.Error("Veritabanı bağlantı hatası: " + sqlEx.Message);
+        }
+        catch (Exception ex)
+        {
+            return Result<List<UsersCommentsDto>>.Error("Bir hata oluştu: " + ex.Message);
         }
     }
 }
