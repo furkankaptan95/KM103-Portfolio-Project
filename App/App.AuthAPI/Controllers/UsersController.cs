646f90f00844a61e61031740f73381a3d1b29105
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-16 19:42:54 +0300

    AuthAPI AdminUserService
    AuthAPI'ye  AdminUserService dahil edildi. AdminServices altındaki IUserService'e yeni metotlar eklendi.

--- App/App.AuthAPI/Controllers/UsersController.cs
+++ App/App.AuthAPI/Controllers/UsersController.cs
@@ -1,17 +1,23 @@
-﻿using App.Data.DbContexts;
+﻿using App.Services.AdminServices.Abstract;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace App.AuthAPI.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class UsersController(AuthApiDbContext authApiDbContext) : ControllerBase
+public class UsersController : ControllerBase
 {
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
     [HttpGet("/get-users-count")]
     public async Task<IActionResult> GetCount()
     {
-        return Ok(await authApiDbContext.Users.CountAsync());
+        return Ok();
     }
 }
