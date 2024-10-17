e832eede7f372e29854a3fc794690939ba9a5054
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-17 16:37:14 +0300

    AdminMVC AboutMe try-catch ve diğer  iyileştirmeler yapıldı.

--- App/App.ViewModels/AdminMvc/Validation/ImageFileValidation.cs
+++ App/App.ViewModels/AdminMvc/Validation/ImageFileValidation.cs
@@ -8,11 +8,10 @@ public class ImageFileValidation : ValidationAttribute
     {
         var file = value as IFormFile;
 
-        // Dosya yüklenmiş mi kontrolü
         var validTypes = new[] { "image/jpeg", "image/png", "image/gif" };
         if (file != null && !validTypes.Contains(file.ContentType))
         {
-            return new ValidationResult(ErrorMessage ?? "Lütfen geçerli bir resim dosyası yükleyiniz."); // Varsayılan mesaj
+            return new ValidationResult(ErrorMessage ?? "Lütfen geçerli bir resim dosyası yükleyiniz.");
         }
 
         return ValidationResult.Success;
