c853f2081a0a0f69cbaa971245febda802d2fc0a
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-17 20:04:35 +0300

    ViewModellerde düzenlemeler yapıldı.

--- App/App.ViewModels/AdminMvc/EducationsViewModels/UpdateEducationViewModel.cs
+++ App/App.ViewModels/AdminMvc/EducationsViewModels/UpdateEducationViewModel.cs
@@ -7,7 +7,7 @@ public class UpdateEducationViewModel
     public int Id { get; set; }
 
     [Required(ErrorMessage = "Derece kısmı zorunludur.")]
-    [MaxLength(100, ErrorMessage = "Derece kısmı en fazla 100 karakter olabilir.")]
+    [MaxLength(50, ErrorMessage = "Derece kısmı en fazla 50 karakter olabilir.")]
     public string Degree { get; set; }
 
     [Required(ErrorMessage = "Okul kısmı zorunludur.")]
