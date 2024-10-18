8836146ce673b6dded62a84c95f835e8a4b150d6
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-19 02:00:01 +0300

    Portfolio Experiences MVC Service tarafı düzenlendi.

--- App/App.PortfolioMVC/Services/ExperiencePortfolioService.cs
+++ App/App.PortfolioMVC/Services/ExperiencePortfolioService.cs
@@ -1,13 +1,33 @@
-﻿using App.DTOs.ExperienceDtos.Admin;
-using App.DTOs.ExperienceDtos.Portfolio;
-using App.Services.PortfolioServices.Abstract;
+﻿using App.Services.PortfolioServices.Abstract;
 using Ardalis.Result;
-
+using App.DTOs.ExperienceDtos.Portfolio;
 namespace App.PortfolioMVC.Services;
-public class ExperiencePortfolioService : IExperiencePortfolioService
+public class ExperiencePortfolioService(IHttpClientFactory factory) : IExperiencePortfolioService
 {
-    public Task<Result<List<DTOs.ExperienceDtos.Portfolio.AllExperiencesPortfolioDto>>> GetAllExperiencesAsync()
+	private HttpClient DataApiClient => factory.CreateClient("dataApi");
+	public async Task<Result<List<AllExperiencesPortfolioDto>>> GetAllExperiencesAsync()
     {
-        throw new NotImplementedException();
-    }
+		try
+		{
+			var apiResponse = await DataApiClient.GetAsync("portfolio-all-experiences");
+
+			if (!apiResponse.IsSuccessStatusCode)
+			{
+				return Result<List<AllExperiencesPortfolioDto>>.Error();
+			}
+			var result = await apiResponse.Content.ReadFromJsonAsync<Result<List<AllExperiencesPortfolioDto>>>();
+
+			if (result is null)
+			{
+				return Result<List<AllExperiencesPortfolioDto>>.Error();
+			}
+
+			return result;
+		}
+
+		catch (Exception)
+		{
+			return Result<List<AllExperiencesPortfolioDto>>.Error();
+		}
+	}
 }
