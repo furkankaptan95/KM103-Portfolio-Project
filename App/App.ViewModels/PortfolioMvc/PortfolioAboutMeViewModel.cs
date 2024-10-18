86d683942968ce764268198841865dcc8fd328be
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-18 23:01:47 +0300

    Porfolio ViewModeller tamamlandı.

---
+++ App/App.ViewModels/PortfolioMvc/PortfolioAboutMeViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace App.ViewModels.PortfolioMvc;
+public class PortfolioAboutMeViewModel
+{
+    public string Introduction { get; set; }
+    public string ImageUrl1 { get; set; }
+    public string ImageUrl2 { get; set; }
+}
---
+++ App/App.ViewModels/PortfolioMvc/PortfolioAllEducationsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace App.ViewModels.PortfolioMvc;
+public class PortfolioAllEducationsViewModel
+{
+    public string Degree { get; set; }
+    public string School { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+}
