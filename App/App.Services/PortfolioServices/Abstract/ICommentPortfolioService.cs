5b8d5ccee3dbcc6864a1530afa4ea8ba5c6f94f2
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-18 23:59:44 +0300

    ICommentPorfolioService eklendi.

---
+++ App/App.Services/PortfolioServices/Abstract/ICommentPortfolioService.cs
@@ -0,0 +1,8 @@
+﻿using App.DTOs.CommentDtos.Portfolio;
+using Ardalis.Result;
+
+namespace App.Services.PortfolioServices.Abstract;
+public interface ICommentPortfolioService
+{
+    Task<Result> AddCommentlAsync(AddCommentDto dto);
+}
