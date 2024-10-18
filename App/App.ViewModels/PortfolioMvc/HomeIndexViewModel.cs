14d1149c750288aa963aff774167c9c7d745096b
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-19 01:05:31 +0300

    PortfolioMVC Education kısmı düzenlendi.

--- App/App.ViewModels/PortfolioMvc/HomeIndexViewModel.cs
+++ App/App.ViewModels/PortfolioMvc/HomeIndexViewModel.cs
@@ -4,11 +4,11 @@ namespace App.ViewModels.PortfolioMvc;
 public class HomeIndexViewModel
 {
     public AboutMePortfolioViewModel? AboutMe { get; set; }
-    public AllEducationsPortfolioViewModel? Educations { get; set; }
-    public AllExperiencesPortfolioViewModel? Experiences { get; set; }
-    public AllProjectsPortfolioViewModel? Projects { get; set; }
+    public List<AllEducationsPortfolioViewModel>? Educations { get; set; }
+    public List<AllExperiencesPortfolioViewModel>? Experiences { get; set; }
+    public List<AllProjectsPortfolioViewModel>? Projects { get; set; }
     public PersonalInfoPortfolioViewModel? PersonalInfo { get; set; }
     public AddContactMessageViewModel? ContactMessage { get; set; }
-    public AllBlogPostsPortfolioViewModel? BlogPosts { get; set; }
+    public List<AllBlogPostsPortfolioViewModel>? BlogPosts { get; set; }
 
 }
