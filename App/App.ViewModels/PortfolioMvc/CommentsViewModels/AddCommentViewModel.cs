bf1febb651664a2cf9df84e2e514dc58b02f4918
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-18 23:08:04 +0300

    AddComment view model düzenlendi ve annotationlar eklendi.

--- App/App.ViewModels/PortfolioMvc/CommentsViewModels/AddCommentViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace App.ViewModels.PortfolioMvc.CommentsViewModels;
-public class AddCommentViewModel
-{
-    public string Content { get; set; }
-    public string? UnsignedCommenterName { get; set; }
-    public int? UserId { get; set; }
-    public int BlogPostId { get; set; }
-}
---
+++ App/App.ViewModels/PortfolioMvc/CommentsViewModels/UnsignedAddCommentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.ViewModels.PortfolioMvc.CommentsViewModels;
+public class UnSignedAddCommentViewModel
+{
+    [Required(ErrorMessage = "İçerik kısmı zorunludur.")]
+    public string Content { get; set; }
+
+    [Required(ErrorMessage = "İsim zorunludur.")]
+    public string UnsignedCommenterName { get; set; }
+    public int BlogPostId { get; set; }
+}
