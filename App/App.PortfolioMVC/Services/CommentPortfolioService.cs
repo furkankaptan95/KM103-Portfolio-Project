eccfd4241b6844ff99f5495a4e437602da26601a
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-19 00:09:34 +0300

    PortfolioMVC Servisler implemente edildi.

---
+++ App/App.PortfolioMVC/Services/CommentPortfolioService.cs
@@ -0,0 +1,12 @@
+﻿using App.DTOs.CommentDtos.Portfolio;
+using App.Services.PortfolioServices.Abstract;
+using Ardalis.Result;
+
+namespace App.PortfolioMVC.Services;
+public class CommentPortfolioService : ICommentPortfolioService
+{
+    public Task<Result> AddCommentlAsync(AddCommentDto dto)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ App/App.PortfolioMVC/Services/PersonalInfoPortfolioService.cs
@@ -0,0 +1,12 @@
+﻿using App.DTOs.PersonalInfoDtos.Portfolio;
+using App.Services.PortfolioServices.Abstract;
+using Ardalis.Result;
+
+namespace App.PortfolioMVC.Services;
+public class PersonalInfoPortfolioService : IPersonalInfoPortfolioService
+{
+    public Task<Result<PersonalInfoPortfolioDto>> GetPersonalInfoAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
