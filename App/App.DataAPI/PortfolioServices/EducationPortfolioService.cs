40c1e553606e2fb33f1cb9ee3a98f9cdafab97e0
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-19 00:23:29 +0300

    Educations DataAPI tarafı düzenlendi.

--- App/App.DataAPI/PortfolioServices/EducationPortfolioService.cs
+++ App/App.DataAPI/PortfolioServices/EducationPortfolioService.cs
@@ -1,12 +1,45 @@
-﻿using App.DTOs.EducationDtos.Portfolio;
+﻿using App.Data.DbContexts;
+using App.DTOs.EducationDtos.Portfolio;
 using App.Services.PortfolioServices.Abstract;
 using Ardalis.Result;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace App.DataAPI.PortfolioServices;
-public class EducationPortfolioService : IEducationPortfolioService
+public class EducationPortfolioService(DataApiDbContext dataApiDb) : IEducationPortfolioService
 {
-    public Task<Result<List<AllEducationsPortfolioDto>>> GetAllEducationsAsync()
+    public async Task<Result<List<AllEducationsPortfolioDto>>> GetAllEducationsAsync()
     {
-        throw new NotImplementedException();
+        try
+        {
+            var dtos = new List<AllEducationsPortfolioDto>();
+
+            var entities = await dataApiDb.Educations.ToListAsync();
+
+            if (entities is null)
+            {
+                return Result<List<AllEducationsPortfolioDto>>.Success(dtos);
+            }
+
+            dtos = entities
+           .Select(item => new AllEducationsPortfolioDto
+           {
+               Degree = item.Degree,
+               School = item.School,
+               StartDate = item.StartDate,
+               EndDate = item.EndDate,
+           })
+           .ToList();
+
+            return Result<List<AllEducationsPortfolioDto>>.Success(dtos);
+        }
+        catch (SqlException sqlEx)
+        {
+            return Result<List<AllEducationsPortfolioDto>>.Error("Veritabanı bağlantı hatası: " + sqlEx.Message);
+        }
+        catch (Exception ex)
+        {
+            return Result<List<AllEducationsPortfolioDto>>.Error("Bir hata oluştu: " + ex.Message);
+        }
     }
 }
