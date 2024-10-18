13aac9056bbb78f2fada63a79492c6eb8513116c
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-18 22:48:54 +0300

    AddCommentViewModel eklendi.

---
+++ App/App.ViewModels/PortfolioMvc/CommentsViewModels/AddCommentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace App.ViewModels.PortfolioMvc.CommentsViewModels;
+public class AddCommentViewModel
+{
+    public string Content { get; set; }
+    public string? UnsignedCommenterName { get; set; }
+    public int? UserId { get; set; }
+    public int BlogPostId { get; set; }
+}
