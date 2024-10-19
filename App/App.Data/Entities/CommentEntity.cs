944bdfd4621b0bd3c2360fea949aa41d85a13af8
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-19 16:54:26 +0300

    DataAPI PortfolioService AddComment metodu düzenlendi.

--- App/App.Data/Entities/CommentEntity.cs
+++ App/App.Data/Entities/CommentEntity.cs
@@ -3,7 +3,7 @@ namespace App.Data.Entities;
 public class CommentEntity : BaseEntity<int>
 {
     public string Content { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
     public bool IsApproved { get; set; } = false;
     public int? BlogPostId { get; set; }
     public BlogPostEntity? BlogPost { get; set; }
--- App/App.DataAPI/Services/PortfolioServices/CommentPortfolioService.cs
+++ App/App.DataAPI/Services/PortfolioServices/CommentPortfolioService.cs
@@ -1,4 +1,5 @@
 ﻿using App.Data.DbContexts;
+using App.Data.Entities;
 using App.DTOs.CommentDtos.Admin;
 using App.DTOs.CommentDtos.Portfolio;
 using App.Services.PortfolioServices.Abstract;
@@ -18,9 +19,35 @@ public class CommentPortfolioService : ICommentPortfolioService
 		_factory = factory;
 	}
 	private HttpClient AuthApiClient => _factory.CreateClient("authApi");
-	public Task<Result> AddCommentAsync(AddCommentDto dto)
+	public async Task<Result> AddCommentAsync(AddCommentDto dto)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var entity = new CommentEntity()
+            {
+              Content = dto.Content,
+			  BlogPostId = dto.BlogPostId,
+			  UnsignedCommenterName = dto.UnsignedCommenterName,
+			  UserId = dto.UserId,
+            };
+
+            await _dataApiDb.Comments.AddAsync(entity);
+            await _dataApiDb.SaveChangesAsync();
+
+            return Result.Success();
+        }
+        catch (DbUpdateException dbUpdateEx)
+        {
+            return Result.Error("Veritabanı güncelleme hatası: " + dbUpdateEx.Message);
+        }
+        catch (SqlException sqlEx)
+        {
+            return Result.Error("Veritabanı bağlantı hatası: " + sqlEx.Message);
+        }
+        catch (Exception ex)
+        {
+            return Result.Error("Bir hata oluştu: " + ex.Message);
+        }
     }
 
 	public async Task<Result<List<BlogPostCommentsPortfolioDto>>> GetBlogPostCommentsAsync(int id)
