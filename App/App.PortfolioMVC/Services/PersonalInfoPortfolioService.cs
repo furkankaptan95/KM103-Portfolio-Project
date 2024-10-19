08663b4e3ad0017e3878c3634d6a1f63aa79ff02
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-19 04:50:58 +0300

    Portfolio PersonalInfo MVC Service tarafı düzenlendi.

--- App/App.PortfolioMVC/Services/PersonalInfoPortfolioService.cs
+++ App/App.PortfolioMVC/Services/PersonalInfoPortfolioService.cs
@@ -1,12 +1,39 @@
 ﻿using App.DTOs.PersonalInfoDtos.Portfolio;
 using App.Services.PortfolioServices.Abstract;
 using Ardalis.Result;
+using System.Net;
 
 namespace App.PortfolioMVC.Services;
-public class PersonalInfoPortfolioService : IPersonalInfoPortfolioService
+public class PersonalInfoPortfolioService(IHttpClientFactory factory) : IPersonalInfoPortfolioService
 {
-    public Task<Result<PersonalInfoPortfolioDto>> GetPersonalInfoAsync()
+	private HttpClient DataApiClient => factory.CreateClient("dataApi");
+	public async Task<Result<PersonalInfoPortfolioDto>> GetPersonalInfoAsync()
     {
-        throw new NotImplementedException();
-    }
+		try
+		{
+			var apiResponse = await DataApiClient.GetAsync("portfolio-get-personal-info");
+
+			if (apiResponse.IsSuccessStatusCode)
+			{
+				var result = await apiResponse.Content.ReadFromJsonAsync<Result<PersonalInfoPortfolioDto>>();
+				if (result is null)
+				{
+					return Result<PersonalInfoPortfolioDto>.Error();
+				}
+				return result;
+			}
+
+			if (apiResponse.StatusCode == HttpStatusCode.NotFound)
+			{
+				return Result<PersonalInfoPortfolioDto>.NotFound();
+			}
+
+			return Result<PersonalInfoPortfolioDto>.Error();
+		}
+
+		catch (Exception)
+		{
+			return Result<PersonalInfoPortfolioDto>.Error();
+		}
+	}
 }
