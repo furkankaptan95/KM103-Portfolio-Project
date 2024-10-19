61ddfdb8eff74a18e93de3c2fcb61358deba7db4
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-19 05:33:49 +0300

    BlogPost Dto,ViewModel ve Serviceler düzenlendi.

---
+++ App/App.DTOs/BlogPostDtos/Porfolio/SingleBlogPostDto.cs
@@ -0,0 +1,11 @@
+﻿using App.DTOs.CommentDtos.Portfolio;
+
+namespace App.DTOs.BlogPostDtos.Porfolio;
+public class SingleBlogPostDto
+{
+	public int Id { get; set; }
+	public string Title { get; set; }
+	public string Content { get; set; }
+	public DateTime PublishDate { get; set; }
+	public List<BlogPostCommentsPortfolioDto>? Comments { get; set; }
+}
--- App/App.DataAPI/Services/PortfolioServices/BlogPosPortfolioService.cs
+++ App/App.DataAPI/Services/PortfolioServices/BlogPosPortfolioService.cs
@@ -1,12 +1,17 @@
-﻿using App.DTOs.BlogPostDtos.Admin;
+﻿using App.DTOs.BlogPostDtos.Porfolio;
 using App.Services.PortfolioServices.Abstract;
 using Ardalis.Result;
 
 namespace App.DataAPI.Services.PortfolioServices;
 public class BlogPosPortfolioService : IBlogPosPortfolioService
 {
-    public Task<Result<List<AllBlogPostsAdminDto>>> GetAllBlogPostsAsync()
-    {
-        throw new NotImplementedException();
-    }
+	public Task<Result<List<AllBlogPostsPortfolioDto>>> GetAllBlogPostsAsync()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Result<SingleBlogPostDto>> GetBlogPostById(int id)
+	{
+		throw new NotImplementedException();
+	}
 }
