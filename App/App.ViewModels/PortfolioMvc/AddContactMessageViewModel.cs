938ed830f5877d2eceeba3f03cc59bad100cc0e8
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-19 20:50:57 +0300

    AddContactMessageViewModel Annotationlara ekleme yapıldı.

--- App/App.ViewModels/PortfolioMvc/AddContactMessageViewModel.cs
+++ App/App.ViewModels/PortfolioMvc/AddContactMessageViewModel.cs
@@ -4,14 +4,15 @@ namespace App.ViewModels.PortfolioMvc;
 public class AddContactMessageViewModel
 {
     [Required(ErrorMessage = "İsim kısmı zorunludur.")]
+    [RegularExpression(@"\S+", ErrorMessage = "İsim kısmı boşluk olamaz.")]
     public string Name { get; set; }
 
     [Required(ErrorMessage = "Email kısmı zorunludur.")]
     [EmailAddress(ErrorMessage = "Geçerli bir email adresi giriniz.")]
     public string Email { get; set; }
-
     public string? Subject { get; set; }
 
     [Required(ErrorMessage = "Mesaj kısmı zorunludur.")]
+    [RegularExpression(@"\S+", ErrorMessage = "Mesaj kısmı boşluk olamaz.")]
     public string Message { get; set; }
 }
