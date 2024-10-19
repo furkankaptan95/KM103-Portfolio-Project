1988bfe4faeee93c4ec76b88938d5d33ffd4b15a
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-19 18:25:29 +0300

    PortfolioMVC Controller Add Actions düzenlendi.

--- App/App.PortfolioMVC/Controllers/CommentController.cs
+++ App/App.PortfolioMVC/Controllers/CommentController.cs
@@ -1,8 +1,10 @@
-﻿using App.ViewModels.PortfolioMvc.CommentsViewModels;
+﻿using App.DTOs.CommentDtos.Portfolio;
+using App.Services.PortfolioServices.Abstract;
+using App.ViewModels.PortfolioMvc.CommentsViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.PortfolioMVC.Controllers;
-public class CommentController : Controller
+public class CommentController(ICommentPortfolioService commentService) : Controller
 {
     [HttpPost]
     public async Task<IActionResult> AddUnsignedComment([FromForm]UnSignedAddCommentViewModel model)
@@ -10,6 +12,24 @@ public class CommentController : Controller
         if (!ModelState.IsValid)
         {
             return Redirect($"/blog-post-{model.BlogPostId}");
+        }
+
+        var dto = new AddCommentUnsignedDto
+        {
+            BlogPostId = model.BlogPostId,
+            UnsignedCommenterName = model.UnsignedCommenterName,
+            Content = model.Content,
+        };
+
+        var result = await commentService.AddCommentUnsignedAsync(dto);
+
+        if (result.IsSuccess)
+        {
+            TempData["Message"] = result.SuccessMessage;
+        }
+        else
+        {
+            TempData["ErrorMessage"] = result.Errors.First();
         }
 
         return Redirect($"/blog-post-{model.BlogPostId}");
@@ -21,6 +41,24 @@ public class CommentController : Controller
         if (!ModelState.IsValid)
         {
             return Redirect($"/blog-post-{model.BlogPostId}");
+        }
+
+        var dto = new AddCommentSignedDto
+        {
+            BlogPostId = model.BlogPostId,
+            UserId = model.UserId,
+            Content = model.Content,
+        };
+
+        var result = await commentService.AddCommentSignedAsync(dto);
+
+        if (result.IsSuccess)
+        {
+            TempData["Message"] = result.SuccessMessage;
+        }
+        else
+        {
+            TempData["ErrorMessage"] = result.Errors.First();
         }
 
         return Redirect($"/blog-post-{model.BlogPostId}");
