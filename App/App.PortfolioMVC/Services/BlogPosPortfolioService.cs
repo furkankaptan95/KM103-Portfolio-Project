216378a0eec6f0d37f6fcd7e8bda634576e5f13c
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-19 06:20:29 +0300

    Portfolio SingleBlogPost MVC Service tarafı düzenlendi.

--- App/App.PortfolioMVC/Services/BlogPosPortfolioService.cs
+++ App/App.PortfolioMVC/Services/BlogPosPortfolioService.cs
@@ -1,17 +1,48 @@
 ﻿using App.DTOs.BlogPostDtos.Porfolio;
 using App.Services.PortfolioServices.Abstract;
 using Ardalis.Result;
+using System.Net;
 
 namespace App.PortfolioMVC.Services;
-public class BlogPosPortfolioService : IBlogPosPortfolioService
+public class BlogPosPortfolioService(IHttpClientFactory factory) : IBlogPosPortfolioService
 {
+	private HttpClient DataApiClient => factory.CreateClient("dataApi");
 	public Task<Result<List<AllBlogPostsPortfolioDto>>> GetAllBlogPostsAsync()
 	{
 		throw new NotImplementedException();
 	}
 
-	public Task<Result<SingleBlogPostDto>> GetBlogPostById(int id)
+	public async Task<Result<SingleBlogPostDto>> GetBlogPostById(int id)
 	{
-		throw new NotImplementedException();
+		try
+		{
+			var apiResponse = await DataApiClient.GetAsync($"portfolio-blog-post-{id}");
+
+			if (apiResponse.IsSuccessStatusCode)
+			{
+				var result = await apiResponse.Content.ReadFromJsonAsync<Result<SingleBlogPostDto>>();
+
+				if (result is null)
+				{
+					return Result<SingleBlogPostDto>.Error();
+				}
+
+				return result;
+			}
+
+			string errorMessage;
+
+			if (apiResponse.StatusCode == HttpStatusCode.NotFound)
+			{
+				return Result<SingleBlogPostDto>.NotFound();
+			}
+
+			return Result<SingleBlogPostDto>.Error();
+		}
+
+		catch (Exception)
+		{
+			return Result<SingleBlogPostDto>.Error();
+		}
 	}
 }
