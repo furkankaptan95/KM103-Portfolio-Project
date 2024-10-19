fd964708b35b4926fcc8e12c664c39dffce37937
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-19 17:55:48 +0300

    Portfolio CommentController Unsigned ve Signed Actionlar düzenlendi.

--- App/App.PortfolioMVC/Controllers/CommentController.cs
+++ App/App.PortfolioMVC/Controllers/CommentController.cs
@@ -5,7 +5,18 @@ namespace App.PortfolioMVC.Controllers;
 public class CommentController : Controller
 {
     [HttpPost]
-    public async Task<IActionResult> AddComment([FromForm]UnSignedAddCommentViewModel model)
+    public async Task<IActionResult> AddUnsignedComment([FromForm]UnSignedAddCommentViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return Redirect($"/blog-post-{model.BlogPostId}");
+        }
+
+        return Redirect($"/blog-post-{model.BlogPostId}");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddSignedComment([FromForm] SignedAddCommentViewModel model)
     {
         if (!ModelState.IsValid)
         {
