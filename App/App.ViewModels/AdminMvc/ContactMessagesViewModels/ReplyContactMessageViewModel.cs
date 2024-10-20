ad1760c2399a34d5d44364fdfba8c0c0be2a38b0
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-20 17:01:08 +0300

    ContactMessage Admin ViewModellar oluşturuldu.

---
+++ App/App.ViewModels/AdminMvc/ContactMessagesViewModels/ReplyContactMessageViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace App.ViewModels.AdminMvc.ContactMessagesViewModels;
+public class ReplyContactMessageViewModel
+{
+    public int MessageId { get; set; }
+    public string Email { get; set; }
+    public string? Subject { get; set; }
+    public string ReplyMessage { get; set; }
+    public string? Message { get; set; }
+    public DateTime? SentDate { get; set; }
+    public string? Name { get; set; }
+
+}
