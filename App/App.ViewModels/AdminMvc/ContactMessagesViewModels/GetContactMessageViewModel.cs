3c38c590f48a7ee6d873f2defaaae12ba6c78178
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-20 23:03:49 +0300

    ReplyModel Post işlemler ve view modellarda düzenlemeler yapıldı.

---
+++ App/App.ViewModels/AdminMvc/ContactMessagesViewModels/GetContactMessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.ViewModels.AdminMvc.ContactMessagesViewModels;
+public class GetContactMessageViewModel
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string? Subject { get; set; }
+    public string Message { get; set; }
+    public DateTime SentDate { get; set; }
+    public string Name { get; set; }
+
+}
---
+++ App/App.ViewModels/AdminMvc/ContactMessagesViewModels/ReplyViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.ViewModels.AdminMvc.ContactMessagesViewModels;
+public class ReplyViewModel
+{
+    public GetContactMessageViewModel? GetModel { get; set; }
+    [Required]
+    public MessageReplyViewModel ReplyModel { get; set; }
+}
