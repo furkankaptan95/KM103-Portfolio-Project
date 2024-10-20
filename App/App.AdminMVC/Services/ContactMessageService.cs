d1cf0cfc76e04e496f6c48a4641163688c6f7d99
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-20 17:55:48 +0300

    AdminMVC Service GetAllContactMessages metodu düzenlendi.

--- App/App.AdminMVC/Services/ContactMessageService.cs
+++ App/App.AdminMVC/Services/ContactMessageService.cs
@@ -6,16 +6,39 @@ using Ardalis.Result;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-public class ContactMessageService : IContactMessageAdminService
+public class ContactMessageService(IHttpClientFactory factory) : IContactMessageAdminService
 {
+    private HttpClient DataApiClient => factory.CreateClient("dataApi");
     public Task<Result> ChangeIsReadAsync(int id)
     {
         throw new NotImplementedException();
     }
 
-    public Task<Result<List<AllContactMessagesDto>>> GetAllContactMessagesAsync()
+    public async Task<Result<List<AllContactMessagesDto>>> GetAllContactMessagesAsync()
     {
-        throw new NotImplementedException();
+        try
+        {
+            var apiResponse = await DataApiClient.GetAsync("all-contact-messages");
+
+            if (!apiResponse.IsSuccessStatusCode)
+            {
+                return Result<List<AllContactMessagesDto>>.Error("Mesajlar getirilirken beklenmedik bir hata oluştu..");
+            }
+
+            var result = await apiResponse.Content.ReadFromJsonAsync<Result<List<AllContactMessagesDto>>>();
+
+            if (result is null)
+            {
+                return Result<List<AllContactMessagesDto>>.Error("Mesajlar getirilirken beklenmedik bir hata oluştu..");
+            }
+
+            return result;
+        }
+
+        catch (Exception)
+        {
+            return Result<List<AllContactMessagesDto>>.Error("Mesajlar getirilirken beklenmedik bir hata oluştu..");
+        }
     }
 
     public Task<Result> ReplyContactMessageAsync(ReplyContactMessageDto dto)
