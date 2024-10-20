f06f4f0160dac53fb683a60e93c630191d2452c7
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-20 16:51:10 +0300

    AdminMVC ContactMessage Controller ve View oluşturuldu.

---
+++ App/App.AdminMVC/Controllers/ContactMessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.AdminMVC.Controllers;
+public class ContactMessageController : Controller
+{
+    [HttpGet]
+    [Route("all-contact-messages")]
+    public async Task<IActionResult> AllContactMessages()
+    {
+        return View();
+    }
+}
