e39116b23850c037c21219255b62f93ca941754f
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-20 19:59:09 +0300

    AdminMVC CMessage GetById Service methodu düzenlendi.

--- App/App.AdminMVC/Services/ContactMessageService.cs
+++ App/App.AdminMVC/Services/ContactMessageService.cs
@@ -1,9 +1,11 @@
 ﻿namespace App.AdminMVC.Services;
 
+using App.DTOs.BlogPostDtos.Admin;
 using App.DTOs.ContactMessageDtos.Admin;
 using App.Services.AdminServices.Abstract;
 using Ardalis.Result;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 
 public class ContactMessageService(IHttpClientFactory factory) : IContactMessageAdminService
@@ -41,9 +43,42 @@ public class ContactMessageService(IHttpClientFactory factory) : IContactMessage
         }
     }
 
-    public Task<Result<ContactMessageToReplyDto>> GetContactMessageByIdAsync(int id)
+    public async Task<Result<ContactMessageToReplyDto>> GetContactMessageByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var apiResponse = await DataApiClient.GetAsync($"get-contact-message-{id}");
+
+            if (apiResponse.IsSuccessStatusCode)
+            {
+                var result = await apiResponse.Content.ReadFromJsonAsync<Result<ContactMessageToReplyDto>>();
+
+                if (result is null)
+                {
+                    return Result<ContactMessageToReplyDto>.Error("Mesaj verisi alınırken bir hata oluştu.");
+                }
+
+                return result;
+            }
+
+            string errorMessage;
+
+            if (apiResponse.StatusCode == HttpStatusCode.NotFound)
+            {
+                errorMessage = "Düzenlemek istediğiniz Mesaj bulunamadı!..";
+            }
+            else
+            {
+                errorMessage = "Mesaj verisi alınırken bir hata oluştu.";
+            }
+
+            return Result<ContactMessageToReplyDto>.Error(errorMessage);
+        }
+
+        catch (Exception)
+        {
+            return Result<ContactMessageToReplyDto>.Error("Mesaj verisi alınırken beklenmedik bir hata oluştu.");
+        }
     }
 
     public Task<Result> ReplyContactMessageAsync(ReplyContactMessageDto dto)
