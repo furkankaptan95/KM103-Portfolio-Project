43b470474e22c4d18640f29707fbe97118383c99
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-21 23:53:57 +0300

    CotactMessage View düzenlemeler iyapıldı.

--- App/App.ViewModels/AdminMvc/ContactMessagesViewModels/MessageReplyViewModel.cs
+++ App/App.ViewModels/AdminMvc/ContactMessagesViewModels/MessageReplyViewModel.cs
@@ -4,8 +4,5 @@ namespace App.ViewModels.AdminMvc.ContactMessagesViewModels;
 public class MessageReplyViewModel
 {
     public int Id { get; set; }
-
-    [Required(ErrorMessage = "Yanıt kısmı zorunludur.")]
-    [RegularExpression(@"^.*\S.*$", ErrorMessage = "Yanıt sadece boşluk olamaz.")]
     public string ReplyMessage { get; set; }
 }
