d93ae89767d2cfe3ea5f11df7ef6eb52d665f536
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-22 20:06:18 +0300

    PersonalInfo Entitye yeni özellikler eklendi ve configürasyonu yapıldı.

--- App/App.Data/Entities/PersonalInfoEntity.cs
+++ App/App.Data/Entities/PersonalInfoEntity.cs
@@ -6,5 +6,8 @@ public class PersonalInfoEntity : BaseEntity<int>
     public string Name { get; set; }
     public string Surname { get; set; }
     public DateTime BirthDate { get; set; }
+    public string Email { get; set; }
+    public string Adress { get; set; }
+    public string Link { get; set; }
 
 }
