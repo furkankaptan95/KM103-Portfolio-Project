c6945b2925f72ae712a2f182ad375477b91286bc
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-23 20:16:53 +0300

    PortfolioMVC Auth Controller oluşturuldu.
    PortfolioMVC Auth Controller oluşturuldu ve register actionları oluşturuldu. RegisterViweModel oluşturuldu.

---
+++ App/App.PortfolioMVC/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.PortfolioMVC.Controllers;
+public class AuthController : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register([FromForm] RegisterViewModel registerModel)
+    {
+        return View();
+    }
+}
