246effa3db4e9a9062f1be123460c63ca0dd6e32
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-24 17:10:04 +0300

    Porfolio Register Formu düzenlendi.

--- App/App.ViewModels/AuthViewModels/RegisterViewModel.cs
+++ App/App.ViewModels/AuthViewModels/RegisterViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace App.ViewModels.AuthViewModels;
 public class RegisterViewModel
 {
+    
     public string Username { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
