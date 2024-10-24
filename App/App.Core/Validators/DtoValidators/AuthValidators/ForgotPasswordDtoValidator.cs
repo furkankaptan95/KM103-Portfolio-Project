fc0505ed1ab52094e7ceda1b25d3696197315b89
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-24 19:40:01 +0300

    ForgotPassword VM ve DTO validatorlar oluşturuldu.

---
+++ App/App.Core/Validators/DtoValidators/AuthValidators/ForgotPasswordDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using App.DTOs.AuthDtos;
+using FluentValidation;
+
+namespace App.Core.Validators.DtoValidators.AuthValidators;
+public class ForgotPasswordDtoValidator : AbstractValidator<ForgotPasswordDto>
+{
+    public ForgotPasswordDtoValidator()
+    {
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email alanı boş bırakılamaz.")
+            .EmailAddress().WithMessage("Geçerli bir email adresi giriniz.")
+            .MaximumLength(100).WithMessage("Email 100 karakterden uzun olamaz.");
+    }
+}
