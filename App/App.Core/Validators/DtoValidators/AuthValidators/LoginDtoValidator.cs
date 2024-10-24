f4ae449f4a2052ec16390cedb6514d037edf3be8
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-24 19:13:53 +0300

    LoginDtoValidator eklendi.

---
+++ App/App.Core/Validators/DtoValidators/AuthValidators/LoginDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using App.DTOs.AuthDtos;
+using FluentValidation;
+
+namespace App.Core.Validators.DtoValidators.AuthValidators;
+public class LoginDtoValidator : AbstractValidator<LoginDto>
+{
+    public LoginDtoValidator()
+    {
+        // Email validasyonu
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email alanı boş bırakılamaz.")
+            .EmailAddress().WithMessage("Geçerli bir email adresi giriniz.")
+            .MaximumLength(100).WithMessage("Email 100 karakterden uzun olamaz.");
+
+        // Şifre validasyonu
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Şifre alanı boş bırakılamaz.")
+            .MinimumLength(8).WithMessage("Şifre en az 8 karakter olmalıdır.")
+            .MaximumLength(15).WithMessage("Şifre en fazla 15 karakter olabilir.");
+    }
+}
