55b37515b253fb2157d85f4523dcbc728f0d1589
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-27 10:03:06 +0300

    LastBlogPostsComponent classı oluşturuldu.

---
+++ App/App.PortfolioMVC/Components/LastBlogPostsComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.PortfolioMVC.Components;
+public class LastBlogPostsComponent : ViewComponent
+{
+	public IViewComponentResult Invoke()
+	{
+
+		return View(); // Görünümü döndürün.
+	}
+}
