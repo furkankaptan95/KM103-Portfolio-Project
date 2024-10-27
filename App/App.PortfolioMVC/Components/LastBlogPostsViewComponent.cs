622bb5d7638088c9c30bfe2af87d257e63a62765
Furkan Kaptan <iamfurkan86@gmail.com>
2024-10-27 11:04:54 +0300

    LastBlogPosts view dosyası düzenlendi.

---
+++ App/App.PortfolioMVC/Components/LastBlogPostsViewComponent.cs
@@ -0,0 +1,44 @@
+﻿using App.Services.PortfolioServices.Abstract;
+using App.ViewModels.PortfolioMvc.BlogPostsViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.PortfolioMVC.Components;
+public class LastBlogPostsViewComponent : ViewComponent
+{
+    private readonly IBlogPostPortfolioService _blogPostPortfolioService;
+
+    public LastBlogPostsViewComponent(IBlogPostPortfolioService blogPostPortfolioService)
+    {
+        _blogPostPortfolioService = blogPostPortfolioService;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var result = await _blogPostPortfolioService.GetHomeBlogPostsAsync();
+
+        var posts = result.Value;
+
+        var latestPosts = posts
+            .OrderByDescending(post => post.PublishDate)
+            .Take(3)
+            .ToList();
+
+        var models = new List<HomeBlogPostsPortfolioViewModel>();
+
+        foreach (var blogPost in latestPosts)
+        {
+            var blogPostToAdd = new HomeBlogPostsPortfolioViewModel
+            {
+                Title = blogPost.Title,
+                Content = blogPost.Content,
+                Id = blogPost.Id,
+                PublishDate = blogPost.PublishDate,
+                CommentsCount = blogPost.CommentsCount
+            };
+
+            models.Add(blogPostToAdd);
+        }
+
+        return View(models);
+    }
+}
